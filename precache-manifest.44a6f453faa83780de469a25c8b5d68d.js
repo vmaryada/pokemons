e0d09efc34da67e945a526485b7c5b11912f1193
Vineeth Reddy Maryada <vmaryada@unomaha.edu>
2019-08-22 10:54:38 -0500

    Updates

---
+++ precache-manifest.44a6f453faa83780de469a25c8b5d68d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "638d80b287bc90c3dd0ac88ad2993183",
+    "url": "/pokemons/index.html"
+  },
+  {
+    "revision": "847490a70246e4c25ffb",
+    "url": "/pokemons/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "8c67421f4678e2dcc228",
+    "url": "/pokemons/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "847490a70246e4c25ffb",
+    "url": "/pokemons/static/js/2.c08aa7e3.chunk.js"
+  },
+  {
+    "revision": "8c67421f4678e2dcc228",
+    "url": "/pokemons/static/js/main.ca2efee1.chunk.js"
+  },
+  {
+    "revision": "cd184ebafb849c792763",
+    "url": "/pokemons/static/js/runtime~main.c8711a1c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokemons/static/media/logo.5d5d9eef.svg"
+  }
+]);
